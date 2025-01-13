c07a6da83e3f0b3cdf53e54ef01c70c315dee3a7
SirGuamsalot <gcardenasnc@icloud.com>
2025-01-13 14:00:56 -0500

    Worked on module 2

---
+++ index.js
@@ -0,0 +1,21 @@
+// JavaScript Code goes here
+console.log("Internal JS console message.")
+console.log(document)
+// console.log(document)
+// alert("Bah humbug")
+// window.onload = function() {
+//     alert("Hello, class")
+// }
+
+//PROMPTS //
+
+const student = prompt("What is your name?")
+console.log(student)
+document.write(`<p>Hello, ${student}!</p}`)
+
+// WRITE TO ELEMENT BY ID
+document.getElementById("myH1").textContent = "I love JavaScript";
+// PAGE STYLING
+document.body.style.backgroundColor = "black"
+document.body.style.color = "lime"
+document.body.style.fontFamily = "helvetica";
