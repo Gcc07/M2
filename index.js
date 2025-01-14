7043594da33025416905246fddf92fdd9ac46225
SirGuamsalot <gcardenasnc@icloud.com>
2025-01-14 13:38:19 -0500

    Assignment 2

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ console.log(document)
 // }
 
 //PROMPTS //
-
+let index = 0
+let changer = ["red", "lime", "orange"]
 const student = prompt("What is your name?")
 console.log(student)
-document.write(`<p>Hello, ${student}!</p}`)
+document.write(`<p>Hello, ${student}!</p>`)
 
 // WRITE TO ELEMENT BY ID
 document.getElementById("myH1").textContent = "I love JavaScript";
@@ -19,3 +20,22 @@ document.getElementById("myH1").textContent = "I love JavaScript";
 document.body.style.backgroundColor = "black"
 document.body.style.color = "lime"
 document.body.style.fontFamily = "helvetica";
+document.getElementById("myH1").addEventListener("click", function(){
+    // function
+    index++
+    if (index >= changer.length) {
+        index = 0
+    }
+    document.getElementById("myH1").style.color = changer[index]
+
+});
+document.getElementById("Butt").addEventListener("click", function(){
+    index++
+    if (index >= changer.length) {
+        index = 0
+    }
+    let classes = document.getElementsByClassName("colorchange")
+    for (let i = 0; i <= changer.length; i++) {
+        classes[i].style.color = changer[index]
+    }
+});
